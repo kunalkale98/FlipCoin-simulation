c5aea8d05a4fed773da3a4fd61d875840cfb6e40
kunalkale98 <kunal,k2898@gmail.com>
2021-02-15 16:28:12 +0530

    Added: Multiple flip to find number of wins

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -x
 #Welcome to Flip Coin Simulation
 
-coin=$((RANDOM%2))
 isHead=1
+n=0
+heads=0
+tails=0
 
-if [ $coin -eq $isHead ]
-	then
-		echo "Heads won"
-else
-	echo "Tails won"
-fi
+while [ $n -le 21 ]
+do
+	coin=$((RANDOM%2))
+	if [ $coin -eq $isHead ]
+		then
+			((heads++))
+	else
+		((tails++))
+	fi
+	((n++))
+done
+
+echo "Heads won "$heads" times"
+echo "Tails won "$tails" times"
